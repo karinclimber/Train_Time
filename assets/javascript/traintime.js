3b783a3aea0133dce567c7c764ba99515ac6b313
Karin Hermann <karinclimber@gmail.com>
2017-07-17 10:19:39 -0400

    update js

--- assets/javascript/traintime.js
+++ assets/javascript/traintime.js
@@ -37,46 +37,57 @@ $("#submit").on("click", function() {
 
 //function to prepend trains
 database.ref().on("child_added", function(childSnapshot){
-	//make sure it works
-	console.log(childSnapshot.val());
 
 	var name = childSnapshot.val().name;
 	var dest = childSnapshot.val().dest;
 	var time = childSnapshot.val().time;
 	var freq = childSnapshot.val().freq;
 
-	console.log("Name: " + name);
-	console.log("Destination: " + dest);
-	console.log("Time: " + time);
-	console.log("Frequency: " + freq);
-	console.log(moment().format("HH:mm"));
-
 //change train time from string to a number
 	var freq = parseInt(freq);
-	//CURRENT TIME
+//current time
 	var currentTime = moment().format('LLLL');
-	console.log("CURRENT TIME: " + moment().format('HH:mm'));
-	//FIRST TIME: PUSHED BACK ONE YEAR TO COME BEFORE CURRENT TIME
-	// var dConverted = moment(time,'hh:mm').subtract(1, 'years');
+
+//current time, minus 1
 	var dConverted = moment(childSnapshot.val().time, 'HH:mm').subtract(1, 'years');
-	console.log("DATE CONVERTED: " + dConverted);
+	
+//train time
 	var trainTime = moment(dConverted).format('HH:mm');
-	console.log("TRAIN TIME : " + trainTime);
 	
 	//math
 	var tConverted = moment(trainTime, 'HH:mm').subtract(1, 'years');
 	var tDifference = moment().diff(moment(tConverted), 'minutes');
-	console.log("DIFFERENCE IN TIME: " + tDifference);
+	
 	//remainder
 	var tRemainder = tDifference % freq;
-	console.log("TIME REMAINING: " + tRemainder);
+	
 	//minutes until the next train
 	var minsAway = freq - tRemainder;
-	console.log("MINUTES UNTIL NEXT TRAIN: " + minsAway);
+	
 	//next train arrival
 	var nextTrain = moment().add(minsAway, 'minutes');
-	console.log("ARRIVAL TIME: " + moment(nextTrain).format('HH:mm A'));
 	
+	
+// $(".form-group").validate({
+// 	rules: {
+// 		nameInput: "required",
+// 		destInput: "required",
+// 		timeInput: {
+// 			require: true,
+// 			number: true
+// 		},
+// 		freqInput: {
+// 			required: true,
+// 			number: true
+// 		}
+// 	},
+// 	messages: {
+// 		nameInput: "Please enter a valid train name!",
+// 		destInput: "Please enter a valid destination!",
+// 		timeInput: "Please enter a valid time, HH:mm.",
+// 		freqInput: "Please enter a valid number."
+// }
+// });
 
 
  //update information in table 
@@ -103,11 +114,14 @@ database.ref().orderByChild("timeAdded").limitToLast(1).on("child_added", functi
     $("#freqDisplay").html(snapshot.val().freq);
 })
 
+//remove train button click function
 $('#removeTrain').on('click', trainTable, function(){
 	//$("#trainTable").closest('tr').remove ();
 	var table = document.getElementById("trainTable");
 	table.deleteRow(table.rows.length-1);
 });
 	
+
+
 
 });
